3d097bd5b9fb3861642a6c776ed9271fffc1e16e
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-27 20:29:44 +0530

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,46 @@
+
+
+# class students:
+#   def __init__(self,name,age):
+#     self.name = name
+#     self.age = age
+
+# obj1 = students("Sajin",21)
+# print(obj1.name)
+# print(obj1.age)
+
+# #O/P
+# '''
+# Sajin
+# 21
+# '''
+
+# class name:
+#   def __init__(self,name,job):
+#     self.name=name
+#     self.job=job
+    
+# obj2 = name("sajin","developer")
+# print(obj2.name)
+# print(obj2.job)
+
+# #O/P 
+# '''
+# sajin
+# developer
+# '''
+
+class classname:
+  def __init__(self,role,id):
+    self.role=role
+    self.id=id
+
+obj3 = classname("developer","123")
+print(obj3.role)
+print(obj3.id)
+
+#O/P 
+'''
+developer
+123
+'''
